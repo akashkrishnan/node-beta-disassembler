be07c0802fd72f19664f65d42fa40f061eb5e846
akashkrishnan <ak@aakay.net>
2014-05-15 09:58:50 -0400

    Fully implemented basic disassembler

--- models/assembler.js
+++ models/assembler.js
@@ -1,14 +1,14 @@
 var Config = require('../config.js');
 
 var optable = [
-  'NOP', 'NOP', 'NOP', 'NOP', 'NOP', 'NOP', 'NOP', 'NOP',
-  'NOP', 'NOP', 'NOP', 'NOP', 'NOP', 'NOP', 'NOP', 'NOP',
-  'NOP', 'NOP', 'NOP', 'NOP', 'NOP', 'NOP', 'NOP', 'NOP',
-  'LD', 'ST', 'NOP', 'JMP', 'BEQ', 'BNE', 'NOP', 'LDR',
-  'ADD', 'SUB', 'MUL', 'DIV', 'CMPEQ', 'CMPLT', 'CMPLE', 'NOP',
-  'AND', 'OR', 'XOR', 'XNOR', 'SHL', 'SHR', 'SRA', 'NOP',
-  'ADDC', 'SUBC', 'MULC', 'DIVC', 'CMPEQC', 'CMPLTC', 'CMPLEC', 'NOP',
-  'ANDC', 'ORC', 'XORC', 'XNORC', 'SHLC', 'SHRC', 'SRAC', 'NOP'
+  null, null, null, null, null, null, null, null,
+  null, null, null, null, null, null, null, null,
+  null, null, null, null, null, null, null, null,
+  'LD', 'ST', null, 'JMP', 'BEQ', 'BNE', null, 'LDR',
+  'ADD', 'SUB', 'MUL', 'DIV', 'CMPEQ', 'CMPLT', 'CMPLE', null,
+  'AND', 'OR', 'XOR', 'XNOR', 'SHL', 'SHR', 'SRA', null,
+  'ADDC', 'SUBC', 'MULC', 'DIVC', 'CMPEQC', 'CMPLTC', 'CMPLEC', null,
+  'ANDC', 'ORC', 'XORC', 'XNORC', 'SHLC', 'SHRC', 'SRAC', null
 ];
 
 module.exports = {
@@ -22,8 +22,8 @@ function disassemble(output, done) {
     var instructions = output.replace(/\s+/g, ' ').split(' ');
     for (var i = 0; i < instructions.length; i++) {
 
-      var instruction = parseInt(instructions[i]);
-      if (instruction) {
+      var instruction = parseInt(instructions[i], 16);
+      if (instruction >= 0) {
 
         var opclass = instruction >>> 30;
         var opcode = extract(instruction, 31, 26);
@@ -32,26 +32,26 @@ function disassemble(output, done) {
         var Rc = reg(extract(instruction, 25, 21));
         var Ra = reg(extract(instruction, 20, 16));
         var Rb = reg(extract(instruction, 15, 11));
-        var literal = hex(extract(instruction, 15, 0));
+        var literal = '0x' + hex(extract(instruction, 15, 0));
 
-        var args = '';
-        if (op !== 'NOP') {
+        var args = '0x' + hex(instruction);
+        if (op) {
           if (opclass == 1) {
             args = Ra + ',' + literal + ',' + Rc;
             if (op === 'ST') args = Rc + ',' + literal + ',' + Ra;
             else if (op === 'JMP') args = Ra + ',' + Rc;
-            else if (op === 'JMP') args = literal + ',' + Rc;
+            else if (op === 'LDR') args = literal + ',' + Rc;
           }
           else if (opclass == 2) args = Ra + ',' + Rb + ',' + Rc;
           else if (opclass == 3) args = Ra + ',' + literal + ',' + Rc;
         }
 
-        assembly += (op ? op : opcode) + '(' + args + ')\n';
+        assembly += (op ? op : 'LONG') + '(' + args + ')\n';
 
       }
 
     }
-    
+
   }
   done(undefined, assembly);
 }
@@ -65,5 +65,5 @@ function reg(r) {
 }
 
 function hex(d) {
-  return '0x' + d.toString(16).toUpperCase();
+  return (d < 0 ? (0xFFFFFFFF + d + 1) : d).toString(16);
 }
